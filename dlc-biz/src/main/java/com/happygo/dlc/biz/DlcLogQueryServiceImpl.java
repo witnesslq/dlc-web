1f70fcf0d6d61277b4155327688febf4970b2f44
xiapshen <1378127237@qq.com>
2017-06-04 09:27:37 +0800

    update dlc-web

---
+++ dlc-biz/src/main/java/com/happygo/dlc/biz/DlcLogQueryServiceImpl.java
@@ -0,0 +1,35 @@
+/**
+* Copyright  2017
+* 
+* All  right  reserved.
+*
+* Created  on  2017年6月4日 上午9:25:42
+*
+* @Package com.happygo.dlc.biz  
+* @Title: DlcLogQueryServiceImpl.java
+* @Description: DlcLogQueryServiceImpl.java
+* @author sxp (1378127237@qq.com) 
+* @version 1.0.0 
+*/
+package com.happygo.dlc.biz;
+
+import java.util.List;
+
+import com.happgo.dlc.base.DlcLog;
+
+/**
+ * ClassName:DlcLogQueryServiceImpl
+ * @Description: DlcLogQueryServiceImpl.java
+ * @author sxp (1378127237@qq.com) 
+ * @date:2017年6月4日 上午9:25:42
+ */
+public class DlcLogQueryServiceImpl implements DlcLogQueryService {
+
+	/* (non-Javadoc)
+	 * @see com.happygo.dlc.biz.DlcLogQueryService#logQuery(java.lang.String)
+	 */
+	public List<DlcLog> logQuery(String keyWord) {
+		
+		return null;
+	}
+}
